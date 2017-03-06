e43d2061da77f52bcfc60ee1e7834787da00640f
Travis Lee <travis.sarles.lee@gmail.com>
2017-03-06 09:32:12 +0100

    Scala charts of commit history

--- src/main/scala/github/Main.scala
+++ src/main/scala/github/Main.scala
@@ -2,10 +2,12 @@ package github
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import org.jfree.data.time.{Day, TimeTableXYDataset}
 
 import scala.collection.mutable.HashMap
 import scala.concurrent.duration._
 import scala.concurrent.Await
+import scalax.chart.api._
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -13,8 +15,24 @@ object Main {
     implicit val materializer = ActorMaterializer()
 
     val github = new Github(args.headOption)
-    println(Await.result(github.fetchUserRepos("scala-native", "scala-native"), 10.seconds))
-    val commitFrequency = new HashMap
+    val commitHistory = Await.result(github.fetchUserRepos("scala-native", "scala-native"), 10.seconds)
+    val commitFrequency = new HashMap[Day, Double]
+    commitHistory.foreach(commitContainer => {
+      val commitDate = commitContainer.commit.author.date
+      val commitDateNoTime = new Day(commitDate.getDayOfMonth, commitDate.getMonthOfYear, commitDate.getYear)
 
+      val count = commitFrequency.get(commitDateNoTime)
+      count match {
+        case Some(previousCount) => commitFrequency.put(commitDateNoTime, previousCount + 1.0)
+        case None => commitFrequency.put(commitDateNoTime, 1.0)
+      }
+    })
+    val data = new TimeTableXYDataset
+
+    for ((date, count) <- commitFrequency) data.add(date, count, "commits")
+
+
+    val chart = XYLineChart(data)
+    chart.saveAsSVG("mychart.svg")
   }
 }
